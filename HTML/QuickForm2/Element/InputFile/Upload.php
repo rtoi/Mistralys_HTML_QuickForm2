90cb02c85a7f2128f3a31b150a4736d836be243a
Mistralys <s.mordziol@gmail.com>
2018-12-16 12:22:54 +0100

    Added exception
    Added an exception when trying to acccess an invalid upload's data.

--- HTML/QuickForm2/Element/InputFile/Upload.php
+++ HTML/QuickForm2/Element/InputFile/Upload.php
@@ -2,6 +2,8 @@
 
 class HTML_QuickForm2_Element_InputFile_Upload
 {
+    const ERROR_CANNOT_RETRIEVE_INVALID_UPLOAD_DATA = 31501;
+    
    /**
     * @var HTML_QuickForm2_Element_InputFile
     */
@@ -12,10 +14,16 @@ class HTML_QuickForm2_Element_InputFile_Upload
     */
     protected $value;
     
+   /**
+    * @var boolean
+    */
+    protected $valid;
+    
     public function __construct(HTML_QuickForm2_Element_InputFile $element, $value)
     {
         $this->element = $element;
         $this->value = $value;
+        $this->valid = !empty($this->value) && is_uploaded_file($this->getTempPath());
     }
     
    /**
@@ -24,39 +32,68 @@ class HTML_QuickForm2_Element_InputFile_Upload
     */
     public function isValid()
     {
-        return !empty($this->value) && is_uploaded_file($this->getTempPath());
+        return $this->valid;
     }
     
    /**
     * The name of the uploaded file, without path.
+    * 
+    * NOTE: Make sure to check if the upload is valid before calling this method.
+    * 
     * @return string
+    * @throws HTML_QuickForm2_Exception
     */
     public function getName()
     {
-        return $this->value['name'];
+        return $this->getKey('name');
+    }
+    
+    protected function getKey($name)
+    {
+        if($this->valid) {
+            return $this->value[$name];
+        }
+        
+        throw new HTML_QuickForm2_Exception(
+            'Cannot retrieve data of an invalid upload.',
+            null,
+            self::ERROR_CANNOT_RETRIEVE_INVALID_UPLOAD_DATA
+        );
     }
     
    /**
     * Retrieves the type of the file, e.g. "image/jpeg".
+    * 
+    * NOTE: Make sure to check if the upload is valid before calling this method.
+    * 
     * @return string
+    * @throws HTML_QuickForm2_Exception
     */
     public function getFileType()
     {
-        return $this->value['type'];
+        return $this->getKey('type');
     }
     
    /**
-    * Retrieves the path to the temporary uploaded file
+    * Retrieves the path to the temporary uploaded file.
+    * 
+    * NOTE: Make sure to check if the upload is valid before calling this method.
+    * 
     * @return string
+    * @throws HTML_QuickForm2_Exception
     */
     public function getTempPath()
     {
-        return $this->value['tmp_name'];
+        return $this->getKey('tmp_name');
     }
     
    /**
     * Retrieves the file size, in bytes.
+    * 
+    * NOTE: Make sure to check if the upload is valid before calling this method.
+    * 
     * @return int
+    * @throws HTML_QuickForm2_Exception
     */
     public function getSize()
     {
@@ -66,8 +103,11 @@ class HTML_QuickForm2_Element_InputFile_Upload
    /**
     * Moves the uploaded file to the target location.
     * 
+    * NOTE: Make sure to check if the upload is valid before calling this method.
+    * 
     * @param string $path
     * @return boolean
+    * @throws HTML_QuickForm2_Exception
     */
     public function moveTo($path)
     {

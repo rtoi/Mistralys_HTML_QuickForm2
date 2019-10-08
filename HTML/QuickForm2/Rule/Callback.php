cbe9765bc19b41dc326a3681fbf1de1e5f1e1776
Mistralys <s.mordziol@gmail.com>
2019-10-08 21:49:29 +0200

    Avoid adding parameter for regular functions

--- HTML/QuickForm2/Rule/Callback.php
+++ HTML/QuickForm2/Rule/Callback.php
@@ -75,12 +75,15 @@ class HTML_QuickForm2_Rule_Callback extends HTML_QuickForm2_Rule
         $value  = $this->owner->getValue();
         $config = $this->getConfig();
        
-        // the rule itself is always the last parameter.
         if(!is_array($config['arguments'])) {
            $config['arguments'] = array();
         }
-       
-        $config['arguments'][] = $this;
+
+        // the rule itself is always the last parameter, except for
+        // regular function callbacks.
+        if(is_array($config['callback'])) {
+            $config['arguments'][] = $this;
+        }
        
         return (bool)call_user_func_array(
             $config['callback'], array_merge(array($value), $config['arguments'])

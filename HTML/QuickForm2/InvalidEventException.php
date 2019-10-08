42c6067905b3bd086e97d45af5c7d88a402ab9b7
Mistralys <s.mordziol@gmail.com>
2019-10-08 21:48:33 +0200

    Added missing exception class.

---
+++ HTML/QuickForm2/InvalidEventException.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Exception classes for HTML_QuickForm2
+ *
+ * PHP version 5
+ *
+ * LICENSE
+ *
+ * This source file is subject to BSD 3-Clause License that is bundled
+ * with this package in the file LICENSE and available at the URL
+ * https://raw.githubusercontent.com/pear/HTML_QuickForm2/trunk/docs/LICENSE
+ *
+ * @category  HTML
+ * @package   HTML_QuickForm2
+ * @author    Alexey Borzov <avb@php.net>
+ * @author    Bertrand Mansion <golgote@mamasam.com>
+ * @copyright 2006-2019 Alexey Borzov <avb@php.net>, Bertrand Mansion <golgote@mamasam.com>
+ * @link      https://pear.php.net/package/HTML_QuickForm2
+ */
+
+/**
+ * Exception that denotes an invalid event name has been passed.
+ *
+ * @category HTML
+ * @package  HTML_QuickForm2
+ * @author   Alexey Borzov <avb@php.net>
+ * @author   Bertrand Mansion <golgote@mamasam.com>
+ * @version  Release: @package_version@
+ * @link     https://pear.php.net/package/HTML_QuickForm2
+ */
+class HTML_QuickForm2_InvalidEventException extends HTML_QuickForm2_Exception
+{
+}

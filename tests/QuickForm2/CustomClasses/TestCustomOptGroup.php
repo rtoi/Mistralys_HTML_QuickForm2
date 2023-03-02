5a3cf088afb98f7d09aa632ce5761a9de5172fb9
Mistralys <s.mordziol@gmail.com>
2023-03-02 10:30:31 +0100

    Added tests.

---
+++ tests/QuickForm2/CustomClasses/TestCustomOptGroup.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace QuickFormTests\CustomClasses;
+
+use HTML_QuickForm2_Element_Select_Optgroup;
+
+class TestCustomOptGroup extends HTML_QuickForm2_Element_Select_Optgroup
+{
+}

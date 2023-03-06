c3f2e003e84cd73ddce4cf3277189c6fc9148d34
Mistralys <s.mordziol@gmail.com>
2023-03-06 16:38:29 +0100

    Restructured PHPStan files.

---
+++ docs/phpstan/run-analysis.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+MemoryLimit=900M
+AnalysisLevel=5
+OutputFile=./result.txt
+ConfigFile=./config.neon
+BinFolder=../../vendor/bin
+
+echo "-------------------------------------------------------"
+echo "RUNNING PHPSTAN @ LEVEL $AnalysisLevel"
+echo "-------------------------------------------------------"
+echo ""
+
+$BinFolder/phpstan analyse -l $AnalysisLevel -c $ConfigFile --memory-limit=$MemoryLimit > $OutputFile

6e903a57618cfa986e381f0ba35dd33c55fe3d00
Mistralys <s.mordziol@gmail.com>
2023-03-11 12:17:52 +0100

    Renamed and namespaced the class #7

--- tests/QuickFormTests/Form/QuickFormTests.php
+++ tests/QuickFormTests/Form/QuickFormTests.php
@@ -10,52 +10,47 @@
  * with this package in the file LICENSE and available at the URL
  * https://raw.githubusercontent.com/pear/HTML_QuickForm2/trunk/docs/LICENSE
  *
- * @category  HTML
  * @package   HTML_QuickForm2
  * @author    Alexey Borzov <avb@php.net>
  * @author    Bertrand Mansion <golgote@mamasam.com>
+ * @category  HTML
  * @copyright 2006-2020 Alexey Borzov <avb@php.net>, Bertrand Mansion <golgote@mamasam.com>
  * @license   https://opensource.org/licenses/BSD-3-Clause BSD 3-Clause License
  * @link      https://pear.php.net/package/HTML_QuickForm2
  */
 
+declare(strict_types=1);
+
+namespace QuickFormTests\Form;
+
+use HTML_QuickForm2;
+use HTML_QuickForm2_DataSource_Array;
+use HTML_QuickForm2_Exception;
+use HTML_QuickForm2_InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
-
-class FormRule extends HTML_QuickForm2_Rule
-{
-    protected function validateOwner()
-    {
-        return false;
-    }
-
-    protected function setOwnerError()
-    {
-        $this->owner->getElementById('foo')->setError('an error message');
-    }
-}
-
+use QuickFormTests\CustomClasses\TestFormRule;
 
 /**
  * Unit test for HTML_QuickForm2 class
  */
-class HTML_QuickForm2Test extends TestCase
+class QuickFormTests extends TestCase
 {
     protected function setUp() : void
     {
         $_REQUEST = array(
             '_qf__track' => ''
         );
-        
+
         $_GET = array(
             'key' => 'value'
         );
-        
+
         $_POST = array();
-        
+
         $_FILES = array();
     }
 
-    public function testTrackSubmit()
+    public function testTrackSubmit() : void
     {
         $tests = array(
             array(
@@ -99,31 +94,31 @@ class HTML_QuickForm2Test extends TestCase
                 'postNotEmpty' => false
             ),
         );
-        
+
         $number = 1;
-        foreach($tests as $def)
+        foreach ($tests as $def)
         {
             $form = new HTML_QuickForm2($def['id'], $def['method'], null, $def['tracking']);
 
             $data = $form->getDataReason();
-            
-            $descr = 'Test #'.$number.' - '.strtoupper($def['method']).': '.$def['label'];
-            
-            $this->assertEquals(
-                $def['count'], 
-                count($form->getDataSources()), 
-                'Datsource count does not match. '.$descr
+
+            $descr = 'Test #' . $number . ' - ' . strtoupper($def['method']) . ': ' . $def['label'];
+
+            $this->assertCount(
+                $def['count'],
+                $form->getDataSources(),
+                'Datasource count does not match. ' . $descr
             );
-            
-            $this->assertEquals($def['trackVarFound'], $data['trackVarFound'], 'Tracking var should have been found. '.$descr);
-            $this->assertEquals($def['postNotEmpty'], $data['postNotEmpty'], 'Post should be empty. '.$descr);
-            $this->assertEquals($def['getNotEmpty'], $data['getNotEmpty'], 'Get should not be empty. '.$descr);
-            
+
+            $this->assertEquals($def['trackVarFound'], $data['trackVarFound'], 'Tracking var should have been found. ' . $descr);
+            $this->assertEquals($def['postNotEmpty'], $data['postNotEmpty'], 'Post should be empty. ' . $descr);
+            $this->assertEquals($def['getNotEmpty'], $data['getNotEmpty'], 'Get should not be empty. ' . $descr);
+
             $number++;
         }
     }
-    
-    public function testConstructorSetsIdAndMethod()
+
+    public function testConstructorSetsIdAndMethod() : void
     {
         $form1 = new HTML_QuickForm2(null);
         $this->assertEquals('post', $form1->getAttribute('method'));
@@ -138,7 +133,7 @@ class HTML_QuickForm2Test extends TestCase
         $this->assertEquals('bar', $form3->getAttribute('id'));
     }
 
-    public function testConstructorSetsDefaultAction()
+    public function testConstructorSetsDefaultAction() : void
     {
         $form1 = new HTML_QuickForm2('test');
         $this->assertEquals($_SERVER['PHP_SELF'], $form1->getAttribute('action'));
@@ -147,44 +142,44 @@ class HTML_QuickForm2Test extends TestCase
         $this->assertEquals('/foobar.php', $form2->getAttribute('action'));
     }
 
-    public function testIdIsReadonly()
+    public function testIdIsReadonly() : void
     {
         $form = new HTML_QuickForm2('foo', 'get');
 
         $this->expectException(HTML_QuickForm2_InvalidArgumentException::class);
-        
+
         $form->removeAttribute('id');
     }
-    
-    public function testMethodIsReadonly()
+
+    public function testMethodIsReadonly() : void
     {
         $form = new HTML_QuickForm2('foo', 'get');
-        
+
         $this->expectException(HTML_QuickForm2_InvalidArgumentException::class);
-        
+
         $form->setAttribute('method', 'post');
     }
-    
-    public function testSetIdIsReadonly()
+
+    public function testSetIdIsReadonly() : void
     {
         $form = new HTML_QuickForm2('foo', 'get');
-        
+
         $this->expectException(HTML_QuickForm2_InvalidArgumentException::class);
-        
+
         $form->setId('newId');
     }
 
-    public function testCannotAddToContainer()
+    public function testCannotAddToContainer() : void
     {
         $form1 = new HTML_QuickForm2('form1');
         $form2 = new HTML_QuickForm2('form2');
 
         $this->expectException(HTML_QuickForm2_Exception::class);
-        
+
         $form1->appendChild($form2);
     }
 
-    public function testSetDataSources()
+    public function testSetDataSources() : void
     {
         $ds1 = new HTML_QuickForm2_DataSource_Array(array('key' => 'value'));
         $ds2 = new HTML_QuickForm2_DataSource_Array(array('another key' => 'foo'));
@@ -198,11 +193,11 @@ class HTML_QuickForm2Test extends TestCase
         $this->assertEquals(2, count($form->getDataSources()));
 
         $this->expectException(HTML_QuickForm2_InvalidArgumentException::class);
-        
+
         $form->setDataSources(array($ds1, 'bogus', $ds2));
     }
 
-    public function testValidateChecksWhetherFormIsSubmitted()
+    public function testValidateChecksWhetherFormIsSubmitted() : void
     {
         $form1 = new HTML_QuickForm2('notrack', 'post');
         $this->assertFalse($form1->validate());
@@ -211,11 +206,11 @@ class HTML_QuickForm2Test extends TestCase
         $this->assertTrue($form2->validate());
     }
 
-    public function testFormRule()
+    public function testFormRule() : void
     {
         $form = new HTML_QuickForm2('track', 'post');
         $foo = $form->addElement('text', 'foo', array('id' => 'foo'));
-        $form->addRule(new FormRule($form));
+        $form->addRule(new TestFormRule($form));
 
         $this->assertFalse($form->validate());
         $this->assertEquals('an error message', $foo->getError());
@@ -225,13 +220,12 @@ class HTML_QuickForm2Test extends TestCase
      * Do not return values for automatically added elements from getValue()
      * @link http://pear.php.net/bugs/bug.php?id=19403
      */
-    public function testRequest19403()
+    public function testRequest19403() : void
     {
         $_POST = array('_qf__track' => '');
-        $form  = new HTML_QuickForm2('track');
+        $form = new HTML_QuickForm2('track');
 
         $this->assertArrayHasKey('_qf__track', $form->getRawValue());
         $this->assertArrayNotHasKey('_qf__track', $form->getValue());
     }
 }
-?>

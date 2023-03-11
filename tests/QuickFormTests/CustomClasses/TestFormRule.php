a442d7c37471a051d42b056eebd57cbb7e8197d2
Mistralys <s.mordziol@gmail.com>
2023-03-11 12:20:31 +0100

    Moved mock out of the test class. #6

---
+++ tests/QuickFormTests/CustomClasses/TestFormRule.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace QuickFormTests\CustomClasses;
+
+use HTML_QuickForm2_Rule;
+
+class TestFormRule extends HTML_QuickForm2_Rule
+{
+    protected function validateOwner() : bool
+    {
+        return false;
+    }
+
+    protected function setOwnerError() : void
+    {
+        $this->owner->getElementById('foo')->setError('an error message');
+    }
+}

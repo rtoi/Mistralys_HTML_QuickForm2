ee2dfbd3a7f67850c96c204620d416c41bf23102
Mistralys <s.mordziol@gmail.com>
2023-06-22 15:12:21 +0200

    Added a base test case.

---
+++ tests/QuickFormTests/CaseClasses/QuickFormCase.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace QuickFormTests\CaseClasses;
+
+use HTML_QuickForm2_Node;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+abstract class QuickFormCase extends TestCase
+{
+
+}

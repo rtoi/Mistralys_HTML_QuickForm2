23f8c2c094dca78322bbcb7e32f8e9328dabfc18
Mistralys <s.mordziol@gmail.com>
2023-06-22 16:10:31 +0200

    Added `setUp()`.

--- tests/QuickFormTests/CaseClasses/QuickFormCase.php
+++ tests/QuickFormTests/CaseClasses/QuickFormCase.php
@@ -4,11 +4,16 @@ declare(strict_types=1);
 
 namespace QuickFormTests\CaseClasses;
 
-use HTML_QuickForm2_Node;
-use PHPUnit\Framework\MockObject\MockObject;
+use HTML_QuickForm2_Rule_Required;
 use PHPUnit\Framework\TestCase;
 
 abstract class QuickFormCase extends TestCase
 {
+    protected function setUp() : void
+    {
+        parent::setUp();
 
+        // Ensure the default message is empty
+        HTML_QuickForm2_Rule_Required::setDefaultMessage('');
+    }
 }

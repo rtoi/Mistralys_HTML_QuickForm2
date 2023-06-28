219bdac48a19c951913df5a738d4f83372ff8e6e
Mistralys <s.mordziol@gmail.com>
2023-06-28 08:48:35 +0200

    Added tests, modernized code.

--- tests/QuickFormTests/Element/InputFileTest.php
+++ tests/QuickFormTests/Element/InputFileTest.php
@@ -10,45 +10,58 @@
  * with this package in the file LICENSE and available at the URL
  * https://raw.githubusercontent.com/pear/HTML_QuickForm2/trunk/docs/LICENSE
  *
- * @category  HTML
  * @package   HTML_QuickForm2
  * @author    Alexey Borzov <avb@php.net>
  * @author    Bertrand Mansion <golgote@mamasam.com>
+ * @category  HTML
  * @copyright 2006-2020 Alexey Borzov <avb@php.net>, Bertrand Mansion <golgote@mamasam.com>
  * @license   https://opensource.org/licenses/BSD-3-Clause BSD 3-Clause License
  * @link      https://pear.php.net/package/HTML_QuickForm2
  */
 
-use PHPUnit\Framework\TestCase;
+declare(strict_types=1);
+
+namespace QuickFormTests\Element;
+
+use HTML_QuickForm2;
+use HTML_QuickForm2_Element_InputFile;
+use HTML_QuickForm2_Element_InputFile_Upload;
+use HTML_QuickForm2_InvalidArgumentException;
+use HTML_QuickForm2_MessageProvider;
+use HTML_QuickForm2_Renderer;
+use QuickFormTests\CaseClasses\QuickFormCase;
 
 /**
- * Unit test for HTML_QuickForm2_Element_InputFile class
+ * @package QuickForm Tests
+ * @see HTML_QuickForm2_Element_InputFile
  */
-class HTML_QuickForm2_Element_InputFileTest extends TestCase
+class InputFileTest extends QuickFormCase
 {
+    // region: Support methods
+
     protected function setUp() : void
     {
         $_FILES = array(
             'foo' => array(
-                'name'      => 'file.doc',
-                'tmp_name'  => '/tmp/nothing',
-                'type'      => 'text/plain',
-                'size'      => 1234,
-                'error'     => UPLOAD_ERR_OK
+                'name' => 'file.doc',
+                'tmp_name' => '/tmp/nothing',
+                'type' => 'text/plain',
+                'size' => 1234,
+                'error' => UPLOAD_ERR_OK
             ),
             'toobig' => array(
-                'name'      => 'ahugefile.zip',
-                'tmp_name'  => '',
-                'type'      => '',
-                'size'      => 0,
-                'error'     => UPLOAD_ERR_FORM_SIZE
+                'name' => 'ahugefile.zip',
+                'tmp_name' => '',
+                'type' => '',
+                'size' => 0,
+                'error' => UPLOAD_ERR_FORM_SIZE
             ),
             'local' => array(
-                'name'      => 'nasty-trojan.exe',
-                'tmp_name'  => '',
-                'type'      => '',
-                'size'      => 0,
-                'error'     => UPLOAD_ERR_CANT_WRITE
+                'name' => 'nasty-trojan.exe',
+                'tmp_name' => '',
+                'type' => '',
+                'size' => 0,
+                'error' => UPLOAD_ERR_CANT_WRITE
             )
         );
         $_POST = array(
@@ -56,7 +69,16 @@ class HTML_QuickForm2_Element_InputFileTest extends TestCase
         );
     }
 
-    public function testCannotBeFrozen()
+    public static function callbackMessageProvider() : string
+    {
+        return "A nasty error happened!";
+    }
+
+    // endregion
+
+    // region: _Tests
+
+    public function testCannotBeFrozen() : void
     {
         $upload = new HTML_QuickForm2_Element_InputFile('foo');
         $this->assertFalse($upload->isFreezable());
@@ -64,7 +86,7 @@ class HTML_QuickForm2_Element_InputFileTest extends TestCase
         $this->assertFalse($upload->toggleFrozen());
     }
 
-    public function testSetValueFromSubmitDataSource()
+    public function testSetValueFromSubmitDataSource() : void
     {
         $form = new HTML_QuickForm2('upload', 'post', null, false);
         $foo = $form->appendChild(new HTML_QuickForm2_Element_InputFile('foo'));
@@ -72,15 +94,31 @@ class HTML_QuickForm2_Element_InputFileTest extends TestCase
 
         $this->assertNull($bar->getValue());
         $this->assertEquals(array(
-            'name'      => 'file.doc',
-            'tmp_name'  => '/tmp/nothing',
-            'type'      => 'text/plain',
-            'size'      => 1234,
-            'error'     => UPLOAD_ERR_OK
+            'name' => 'file.doc',
+            'tmp_name' => '/tmp/nothing',
+            'type' => 'text/plain',
+            'size' => 1234,
+            'error' => UPLOAD_ERR_OK
         ), $foo->getValue());
     }
 
-    public function testBuiltinValidation()
+    public function testGetUploadInstanceValidatesFile() : void
+    {
+        $form = new HTML_QuickForm2('upload', 'post', null, false);
+        $foo = new HTML_QuickForm2_Element_InputFile('foo');
+        $form->appendChild($foo);
+
+        $upload = $foo->getUpload();
+
+        // This upload is invalid, because it's only simulated.
+        $this->assertFalse($upload->isValid());
+
+        $this->expectExceptionCode(HTML_QuickForm2_Element_InputFile_Upload::ERROR_CANNOT_RETRIEVE_INVALID_UPLOAD_DATA);
+
+        $upload->getName();
+    }
+
+    public function testBuiltinValidation() : void
     {
         $form = new HTML_QuickForm2('upload', 'post', null, false);
         $form->appendChild(new HTML_QuickForm2_Element_InputFile('foo'));
@@ -91,21 +129,47 @@ class HTML_QuickForm2_Element_InputFileTest extends TestCase
         $this->assertStringContainsString('987654', $toobig->getError());
     }
 
-    public function testInvalidMessageProvider()
+    public function testInvalidMessageProvider() : void
     {
         $this->expectException(HTML_QuickForm2_InvalidArgumentException::class);
-        
+
         new HTML_QuickForm2_Element_InputFile('invalid', null, array('messageProvider' => array()));
     }
 
-    public static function callbackMessageProvider($messageId, $langId)
+    public function testAddAccept() : void
     {
-        return "A nasty error happened!";
+        $el = new HTML_QuickForm2_Element_InputFile('foo');
+        $el->addAccept('text/plain');
+
+        $this->assertSame('text/plain', $el->getAccept());
     }
 
-    public function testCallbackMessageProvider()
+    public function testAddAccepts() : void
     {
-        $form   = new HTML_QuickForm2('upload', 'post', null, false);
+        $el = new HTML_QuickForm2_Element_InputFile('foo');
+        $el->addAccepts(array('text/plain', 'image/png', 'application/json'));
+
+        // Accept mimes are sorted alphabetically
+        $this->assertSame('application/json,image/png,text/plain', $el->getAccept());
+        $this->assertSame(array('application/json', 'image/png', 'text/plain'), $el->getAcceptMimes());
+    }
+
+    /**
+     * A file upload element must be attached to a form to be
+     * rendered or submitted, or to fetch its value.
+     */
+    public function testFormIsRequiredToRender() : void
+    {
+        $el = new HTML_QuickForm2_Element_InputFile('foo');
+
+        $this->expectExceptionCode(HTML_QuickForm2_Element_InputFile::ERROR_ELEMENT_HAS_NO_FORM);
+
+        $el->renderToArray();
+    }
+
+    public function testCallbackMessageProvider() : void
+    {
+        $form = new HTML_QuickForm2('upload', 'post', null, false);
         $upload = $form->addFile('local', array(), array(
             'messageProvider' => array(__CLASS__, 'callbackMessageProvider')
         ));
@@ -113,82 +177,90 @@ class HTML_QuickForm2_Element_InputFileTest extends TestCase
         $this->assertEquals('A nasty error happened!', $upload->getError());
     }
 
-    public function testObjectMessageProvider()
+    public function testObjectMessageProvider() : void
     {
-        $mockProvider = $this->getMockBuilder('HTML_QuickForm2_MessageProvider')
-            ->setMethods(array('get'))
+        $mockProvider = $this
+            ->getMockBuilder(HTML_QuickForm2_MessageProvider::class)
+            ->onlyMethods(array('get'))
             ->getMock();
-        $mockProvider->expects($this->once())->method('get')
-                     ->will($this->returnValue('A nasty error happened!'));
 
-        $form   = new HTML_QuickForm2('upload', 'post', null, false);
+        $mockProvider
+            ->expects($this->once())
+            ->method('get')
+            ->willReturn('A nasty error happened!');
+
+        $form = new HTML_QuickForm2('upload', 'post', null, false);
         $upload = $form->addFile('local', array(), array(
             'messageProvider' => $mockProvider
         ));
+
         $this->assertFalse($form->validate());
         $this->assertEquals('A nasty error happened!', $upload->getError());
     }
 
-   /**
-    * File should check that the form has POST method, set enctype to multipart/form-data
-    * @see http://pear.php.net/bugs/bug.php?id=16807
-    */
-    public function testRequest16807()
+    /**
+     * File should check that the form has POST method, set enctype to multipart/form-data
+     * @see http://pear.php.net/bugs/bug.php?id=16807
+     */
+    public function testRequest16807() : void
     {
         $form = new HTML_QuickForm2('broken1', 'get');
 
-        $this->expectException(HTML_QuickForm2_InvalidArgumentException::class);
-        
         $upload = $form->addFile('upload', array('id' => 'upload'));
-        
+
+        $this->expectExceptionCode(HTML_QuickForm2::ERROR_MULTIPART_REQUIRES_POST);
+
         $upload->getValue();
     }
-    
-   /**
-    * Ensure that the form action is checked on retrieving the value
-    */
-    public function testRequest16807_2()
+
+    /**
+     * Ensure that the form action is checked on retrieving the value
+     */
+    public function testRequest16807_2() : void
     {
         $form = new HTML_QuickForm2('broken2', 'get');
-        
+
         /* HTML_QuickForm2_Container_Group */
         $group = $form->addElement('group', 'fileGroup');
 
         $upload = $group->addFile('upload', array('id' => 'upload'));
-        
-        $this->expectException(HTML_QuickForm2_InvalidArgumentException::class);
-        
+
+        $this->expectExceptionCode(HTML_QuickForm2::ERROR_MULTIPART_REQUIRES_POST);
+
         $upload->getValue();
     }
-    
-   /**
-    * Ensure that the form action is checked on render
-    */
-    public function testRequest16807_3()
+
+    /**
+     * Ensure that the form action is checked on render
+     */
+    public function testRequest16807_3() : void
     {
         $form = new HTML_QuickForm2('broken2', 'get');
-        
+
         $form->addFile('upload', array('id' => 'upload'));
-        
-        $this->expectException(HTML_QuickForm2_InvalidArgumentException::class);
-        
+
+        $this->expectExceptionCode(HTML_QuickForm2::ERROR_MULTIPART_REQUIRES_POST);
+
         $renderer = HTML_QuickForm2_Renderer::factory('Array');
-        
+
         $form->render($renderer);
     }
-    
-    public function testRequest16807_4()
+
+    public function testRequest16807_4() : void
     {
         $post = new HTML_QuickForm2('okform', 'post');
-        
+
         $this->assertNull($post->getAttribute('enctype'));
-        
+
         $upload = $post->addFile('upload');
-        
+
         // the check is done whenever the value is accessed,
         // or the form is validated / rendered.
         $upload->getValue();
-        
+
         $this->assertEquals('multipart/form-data', $post->getAttribute('enctype'));
+        $this->assertTrue($post->isMultiPart());
     }
+
+    // endregion
 }
